9e92b8b9b8dff1fca1be88f2c02d6e7a7404674b
steven.dev <herstevendev@gmail.com>
2024-06-05 09:47:54 -0300

    pude hacer que se rendericen los iconos de las redes sociales y corregi e hice links los p que estan a la derecha del footer

---
+++ src/components/CardsInspiracion.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CardsInspiracion = () => {
+  return (
+    <div className='mt-12 container'>
+        <h2 className='text-4xl font-bold text-gray-800 mb-8'>Inspirate para tu proximo viaje</h2>
+    </div>
+  )
+}

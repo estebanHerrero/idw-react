f933e72a800776dfd89d75795adf97a43b4cde97
Paola <88917727+Paola0015@users.noreply.github.com>
2024-06-13 16:16:47 -0300

    borre de node_modules esto node_modules/@esbuild/win32-x64/esbuild.exe y node_modules/@rollup/rollup-win32-x64-msvc/rollup.win32-x64-msvc.node, que no me dejaba hacer el pasaje de la rama main a la mia

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,32 +1,69 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendarAlt, faEarthAmericas, faSearch, faBed } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCalendarAlt,
+  faEarthAmericas,
+  faSearch,
+  faBed,
+} from "@fortawesome/free-solid-svg-icons";
 
-export function SearchBar() {
-    return (
-        <div className="bg-[#F6FDC3] px-4 py-6 flex justify-center">
-            <div className='bg-[#CDFADB] px-4 py-2 flex items-center justify-between w-[1000px] rounded-xl'>
-                {/* Sección 1: Donde queres ir? */}
-                <div className="flex-shrink-0 bg-[#FFCF96] p-2 px-8 rounded">
-                    <span className="text-gray-700 font-light">¿Dónde quieres ir?</span>
-                    <FontAwesomeIcon icon={faEarthAmericas} className="text-gray-500 ml-2" />
-                </div>
+const SearchBar = ({ onSearch }) => {
+  const [query, setQuery] = useState("");
 
-                {/* Sección 2: Fechas */}
-                <div className="flex-shrink-0 bg-[#FFCF96] p-2 px-8 rounded">
-                    <span className="text-gray-700 font-light">19 Abril Vier -- 26 Abril Vie</span>
-                    <FontAwesomeIcon icon={faCalendarAlt} className="text-gray-500 ml-4" />
-                </div>
+  const handleSearchClick = () => {
+    onSearch(query);
+  };
 
-                {/* Sección 3: Personas y Habitaciones */}
-                <div className="flex-shrink-0 bg-[#FFCF96] p-2 px-8 rounded">
-                    <span className="text-gray-700 font-light">1 Adulto - 1 Niño - 2 Dor</span>
-                    <FontAwesomeIcon icon={faBed} className="text-gray-500 ml-4" />
-                </div>
-
-                {/* Sección 4: Buscar */}
-                <button className="bg-[#FF8080] text-gray-700 font-light px-6 py-2 rounded">Buscar <FontAwesomeIcon icon={faSearch} className="text-gray-500 ml-4" /></button>
-            </div>
+  return (
+    <div className="bg-[#F6FDC3] px-4 py-6 flex justify-center">
+      <div className="bg-[#CDFADB] px-4 py-2 flex items-center justify-between w-[1000px] rounded-xl">
+        {/* Sección 1: Donde queres ir? */}
+        <div className="flex-shrink-0 bg-[#FFCF96] p-2 px-8 rounded">
+          <span className="text-gray-700 font-light">¿Dónde quieres ir?</span>
+          <FontAwesomeIcon
+            icon={faEarthAmericas}
+            className="text-gray-500 ml-2"
+          />
+          <input
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            className="ml-2 p-1"
+            placeholder="Busca tu destino."
+          />
         </div>
-    )
-}
+
+        {/* Sección 2: Fechas */}
+        <div className="flex-shrink-0 bg-[#FFCF96] p-2 px-8 rounded">
+          <span className="text-gray-700 font-light">
+            19 Abril Vier -- 26 Abril Vie
+          </span>
+          <FontAwesomeIcon
+            icon={faCalendarAlt}
+            className="text-gray-500 ml-4"
+          />
+        </div>
+
+        {/* Sección 3: Personas y Habitaciones */}
+        <div className="flex-shrink-0 bg-[#FFCF96] p-2 px-8 rounded">
+          <span className="text-gray-700 font-light">
+            1 Adulto - 1 Niño - 2 Dor
+          </span>
+          <FontAwesomeIcon icon={faBed} className="text-gray-500 ml-4" />
+        </div>
+
+        {/* Sección 4: Buscar */}
+
+        <button
+          className="bg-[#FF8080] text-gray-700 font-light px-6 py-2 rounded"
+          onClick={handleSearchClick}
+        >
+          Buscar{" "}
+          <FontAwesomeIcon icon={faSearch} className="text-gray-500 ml-4" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
